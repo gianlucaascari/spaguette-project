8c10b44cb591350e095d9c91f31f087c46306fab
gianlucaascari <148340519+gianlucaascari@users.noreply.github.com>
2025-02-07 11:50:25 +0100

    cleaned imports

--- spaguette-app/app/index.tsx
+++ spaguette-app/app/index.tsx
@@ -1,9 +1,6 @@
-import React, { useContext, useEffect } from 'react'
+import React from 'react'
 import { Redirect } from 'expo-router'
-import { ActivityIndicator, Text } from 'react-native'
 import { useAuthService } from '@/services/auth/auth-service'
-import { AuthContext } from '@/services/auth/AuthContext'
-import { View } from '@/components/Themed'
 
 const index = () => {
   const authService = useAuthService()
