0658009181eafc2e05ef9f286282a5108e7ea88a
gianlucaascari <148340519+gianlucaascari@users.noreply.github.com>
2025-02-20 16:47:13 +0100

    written localStorageService for ingredients

--- spaguette-app/services/local-storage/local-storage-catalogue-service.ts
+++ spaguette-app/services/local-storage/local-storage-catalogue-service.ts
@@ -1,6 +1,7 @@
 import { Ingredient } from "@/types/application/Catalogue";
 import { database } from "./database";
 import { DbIngredient } from "@/types/database/Catalogue";
+import { Q } from "@nozbe/watermelondb";
 
 const localStorageCatalogueService = {
     getIngredients: async (): Promise<Ingredient[]> => {
@@ -10,6 +11,43 @@ const localStorageCatalogueService = {
             name: i.name,
             unityOfMeasure: i.unityOfMeasure,
         }));
+    },
+    addIngredient: async (ingredient: Ingredient): Promise<void> => {
+        const existingIngredient = await database.get<DbIngredient>("ingredients").query(Q.where("remote_id", ingredient.id)).fetch();
+        if (existingIngredient.length > 0) {
+            throw new Error("Ingredient already exists");
+        }
+
+        await database.write(async () => {
+            await database.get<DbIngredient>("ingredients").create((i) => {
+                i.remoteId = ingredient.id;
+                i.name = ingredient.name;
+                i.unityOfMeasure = ingredient.unityOfMeasure;
+            });
+        });
+    },
+    updateIngredient: async (ingredient: Ingredient): Promise<void> => {
+        await database.write(async () => {
+            const dbIngredient = await database.get<DbIngredient>("ingredients").query(Q.where("remote_id", ingredient.id)).fetch();
+            if (dbIngredient.length === 0) {
+                throw new Error("Ingredient not found");
+            }
+
+            await dbIngredient[0].update((i) => {
+                i.name = ingredient.name;
+                i.unityOfMeasure = ingredient.unityOfMeasure;
+            });
+        });
+    },
+    deleteIngredient: async (id: string): Promise<void> => {
+        await database.write(async () => {
+            const dbIngredient = await database.get<DbIngredient>("ingredients").query(Q.where("remote_id", id)).fetch();
+            if (dbIngredient.length === 0) {
+                throw new Error("Ingredient not found");
+            }
+
+            await dbIngredient[0].destroyPermanently();
+        });
     },
 };
 
