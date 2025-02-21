fe21d3f22892a82b6acc3665ead2b58458056b71
gianlucaascari <148340519+gianlucaascari@users.noreply.github.com>
2025-02-21 09:28:03 +0100

    working ios, android and web

--- spaguette-app/app/(protected)/(tabs)/test-page.tsx
+++ spaguette-app/app/(protected)/(tabs)/test-page.tsx
@@ -1,16 +1,28 @@
 import { View } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
 import Button from "@/components/general/Button";
 import { localStoragePlanService } from "@/services/local-storage/local-storage-plan-service";
 import localStorageCatalogueService from "@/services/local-storage/local-storage-catalogue-service";
+import { DataContext } from "@/services/data/DataContext";
 
 const TestPage = () => {
+
+  const { state } = useContext(DataContext);
 
   const onRead = async () => {
     try {
       // const list = await localStoragePlanService.getMyList();
-      const list = await localStorageCatalogueService.getIngredients();
+      const list = await localStoragePlanService.getMyList();
       console.log(JSON.stringify(list, null, 2));
+    } catch (error) {
+        alert("Errore: " + error);
+    }
+  };
+
+  const onWrite = async () => {
+    try {
+      await localStorageCatalogueService.addIngredient(state.ingredients[0])
+      alert("Ingredient added");
     } catch (error) {
         alert("Errore: " + error);
     }
@@ -18,6 +30,7 @@ const TestPage = () => {
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Button text="write" style="primary" onPress={onWrite} />
       <Button text="read" style="primary" onPress={onRead} />
     </View>
   );
