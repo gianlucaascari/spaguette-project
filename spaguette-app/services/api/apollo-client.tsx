24ad19422cb50d23a08eeda9c6aed15682bebcf5
gianlucaascari <148340519+gianlucaascari@users.noreply.github.com>
2025-03-02 22:57:48 +0100

    readd ws

--- spaguette-app/services/api/apollo-client.tsx
+++ spaguette-app/services/api/apollo-client.tsx
@@ -1,8 +1,8 @@
 import { split, ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from '@apollo/client/link/context';
-// import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
-// import { createClient } from 'graphql-ws';
-// import { getMainDefinition } from "@apollo/client/utilities";
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { createClient } from 'graphql-ws';
+import { getMainDefinition } from "@apollo/client/utilities";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { persistCache } from "apollo3-cache-persist";
 
@@ -29,17 +29,17 @@ const httpLink = new HttpLink({
   // uri: "https://7afc-193-190-253-145.ngrok-free.app",
 });
 
-// const wsLink = new GraphQLWsLink(createClient({
-//    //url: "http://localhost:4000/subscriptions",
-//    url: "https://7afc-193-190-253-145.ngrok-free.app/subscriptions",
-//   connectionParams: async () => {
-//     const authUserString = await AsyncStorage.getItem('authUser')
-//     const authUser = authUserString ? JSON.parse(authUserString) : {} 
-//     return {
-//       authorization: authUser.token || '',
-//     }
-//   }
-// }))
+const wsLink = new GraphQLWsLink(createClient({
+   //url: "http://localhost:4000/subscriptions",
+   url: "https://7afc-193-190-253-145.ngrok-free.app/subscriptions",
+  connectionParams: async () => {
+    const authUserString = await AsyncStorage.getItem('authUser')
+    const authUser = authUserString ? JSON.parse(authUserString) : {} 
+    return {
+      authorization: authUser.token || '',
+    }
+  }
+}))
 
 const authLink = setContext(async (_, { headers }) => {
   const authUserString = await AsyncStorage.getItem('authUser');
@@ -52,21 +52,21 @@ const authLink = setContext(async (_, { headers }) => {
   };
 });
 
-// const splitLink = split(
-//   ({ query }) => {
-//     const definition = getMainDefinition(query);
-//     return (
-//       definition.kind === 'OperationDefinition' &&
-//       definition.operation === 'subscription'
-//     );
-//   },
-//   wsLink,
-//   authLink.concat(httpLink),
-// );
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink),
+);
 
 const client = new ApolloClient({
-  // link: splitLink,
-  link: authLink.concat(httpLink),
+  link: splitLink,
+  // link: authLink.concat(httpLink),
   cache,
 });
 
